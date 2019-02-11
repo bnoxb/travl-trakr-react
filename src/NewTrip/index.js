d5a0c532b8c4aed1d49e56c1cbbea73b329f8836
Tim Mandel <timothy.mandel@gmail.com>
2019-02-11 12:47:45 -0600

    New Trip form added

--- src/NewTrip/index.js
+++ src/NewTrip/index.js
@@ -12,8 +12,23 @@ class NewTrip extends Component {
 
 	render() {
 		return (
-			<div>NewTrip</div>
+			<div>
+				<div>NewTrip</div>
+				<form>
+					<input type='text' name='name' onChange={this.handleChange} /><br/>
+					<label>
+						Date of Arrival
+						<input type='date' name='dateArrived' onChange={this.handleChange} />
+					</label><br/>
+					<label>
+						Date of Left
+						<input type='date' name='dateLeft' onChange={this.handleChange} />
+					</label><br/>
+					<label>Some notes about your trip:</label><br/>
+					<textarea name='notes' rows='8' columns='50'></textarea>
 
+				</form>
+			</div>
 		)
 	}
 }
